458b2e874d45ec60e101275de61214499146b116
Priscila Neiva <tecnico.priscila.neiva@gmail.com>
2026-06-22 20:24:43 -0300

    Modularização de API

---
+++ AppGestaoBrasileirao/frmLogin.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AppGestaoBrasileirao
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            btnSair = new Button();
+            btnEntrar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.Captura_de_tela_2026_06_15_204310;
+            pictureBox1.Location = new Point(31, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(388, 371);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkBlue;
+            label1.Location = new Point(490, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(298, 195);
+            label1.TabIndex = 1;
+            label1.Text = "Sistema de\r\ncontrole de \r\n          times\r\n";
+            label1.Click += this.label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkBlue;
+            panel1.Controls.Add(btnEntrar);
+            panel1.Controls.Add(btnSair);
+            panel1.Location = new Point(0, 341);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(801, 109);
+            panel1.TabIndex = 2;
+            // 
+            // btnSair
+            // 
+            btnSair.BackColor = Color.Red;
+            btnSair.FlatAppearance.BorderColor = Color.Black;
+            btnSair.FlatStyle = FlatStyle.Flat;
+            btnSair.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSair.ForeColor = SystemColors.ButtonHighlight;
+            btnSair.Location = new Point(490, 33);
+            btnSair.Name = "btnSair";
+            btnSair.Size = new Size(125, 46);
+            btnSair.TabIndex = 0;
+            btnSair.Text = "Sair";
+            btnSair.TextAlign = ContentAlignment.MiddleRight;
+            btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.BackColor = Color.FromArgb(0, 192, 0);
+            btnEntrar.FlatAppearance.BorderColor = Color.Black;
+            btnEntrar.FlatStyle = FlatStyle.Flat;
+            btnEntrar.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.ForeColor = SystemColors.ButtonHighlight;
+            btnEntrar.Location = new Point(635, 33);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(125, 48);
+            btnEntrar.TabIndex = 1;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.TextAlign = ContentAlignment.MiddleRight;
+            btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panel1;
+        private Button btnEntrar;
+        private Button btnSair;
+    }
+}
---
+++ AppGestaoBrasileirao/frmLogin.cs
@@ -0,0 +1,10 @@
+namespace AppGestaoBrasileirao
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
