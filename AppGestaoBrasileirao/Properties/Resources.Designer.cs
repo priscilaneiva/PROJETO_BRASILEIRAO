045097046d8f0a7a0e1890109234b8841b540bc6
Priscila Neiva <tecnico.priscila.neiva@gmail.com>
2026-06-22 21:13:47 -0300

    Tela Cosultar nop Forms

--- AppGestaoBrasileirao/Properties/Resources.Designer.cs
+++ AppGestaoBrasileirao/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace AppGestaoBrasileirao.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap jogador {
+            get {
+                object obj = ResourceManager.GetObject("jogador", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
